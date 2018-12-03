9e2cae7e717e77af31582d854d3fe1fd078a7f0f
Tiago-PC <Tiago-PC@DESKTOP-AKQH14S>
2018-12-03 21:09:18 -0200

    Commit. Tiago, Odirlei, Juliana, Everton

--- src/main/java/gui/GuiCliente.java
+++ src/main/java/gui/GuiCliente.java
@@ -12,7 +12,6 @@ import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
-import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -44,7 +43,7 @@ public class GuiCliente implements Serializable {
     public String novoCliente() {
         cliente = new Cliente();
         incluindo = true;
-        return "CadCLientes";
+        return "CadClientes";
 
     }
 
