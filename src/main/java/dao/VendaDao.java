2c473779b5b436b39eb598ed92df56c030397cc9
Tiago <Tiago-PC@DESKTOP-AKQH14S>
2018-12-08 13:57:01 -0200

    Commit. Tiago

--- src/main/java/dao/VendaDao.java
+++ src/main/java/dao/VendaDao.java
@@ -5,10 +5,37 @@
  */
 package dao;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import model.ItemCompra;
+import model.Venda;
+
 /**
  *
  * @author Tiago-PC
  */
 public class VendaDao {
-    
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void incluir(Venda venda) {
+        em.persist(venda);
+    }
+
+    public void alterar(Venda venda) {
+        em.merge(venda);
+    }
+
+    public void excluir(Venda venda) {
+        em.remove(em.merge(venda));
+    }
+
+    public List<ItemCompra> getItensVenda() {
+        Query q = em.createQuery("Select  v from Compra  v ");
+        return q.getResultList();
+    }
+
 }
